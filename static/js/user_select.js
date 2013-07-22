9c5233ec9f255dabb7f6b8ecb641fb483989d0bd
Ryan Diaz <diazry@gmail.com>
2013-07-22 11:02:46 -0700

    checkbox update

--- static/js/user_select.js
+++ static/js/user_select.js
@@ -1,17 +1,13 @@
 $(document).ready(function() {
-    console.log("checkbox script loaded");
+    $('.leaderCheckbox').hide();
     $('.memberCheckbox').click(function() {
         var selected = [];
-        console.log("eeeeeee");
         $('#memberSelect input:checked').each(function(){
-            console.log($(this).attr('name'));
             selected.push($(this).val());
         });
-        console.log(selected[0]);
         $('#leaderSelect input:checkbox').each(function(){
             var showIt = false;
             for (var i = selected.length - 1; i >= 0; i--) {
-                console.log(selected[i]);
                  if($(this).val() == selected[i]){
                     showIt = true;
                     break;
